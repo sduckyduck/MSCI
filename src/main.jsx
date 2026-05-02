f055304f954e66b68bc62b033368cf701f749fed
sduckyduck <hqywcyhqf@gmail.com>
2026-05-02 14:12:44 -0400

    Load gender-only character controls

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import "./character-builder.css";
 import "./premium-light.css";
 import "./export-stable-preview.css";
 import "./stable-character-preview-manager.js";
+import "./gender-only-character-controls.js";
 import "./intro-layout-cleanup.js";
 import "./simplify-random-button.js";
 import "./safe-result-export.js";
