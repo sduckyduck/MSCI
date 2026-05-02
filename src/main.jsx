2ba806f0e618aa779a330099bf2bd49094352e38
sduckyduck <hqywcyhqf@gmail.com>
2026-05-02 13:29:52 -0400

    Load final preview action guard

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import "./msio-safe-action-fix.js";
 import "./msci-class-visual-fix.js";
 import "./archer-weapon-action-fix.js";
 import "./mage-weapon-shield-fix.js";
+import "./final-preview-action-guard.js";
 import "./simplify-random-button.js";
 import "./safe-result-export.js";
 
