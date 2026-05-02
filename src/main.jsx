1559a1209485f28136eaf67d16081ee1919bb01a
sduckyduck <hqywcyhqf@gmail.com>
2026-05-02 14:26:43 -0400

    Remove unused result page DOM observers

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import "./compact-text.css";
 import "./character-builder.css";
 import "./premium-light.css";
 import "./export-stable-preview.css";
-import "./gender-only-character-controls.js";
 import "./intro-layout-cleanup.js";
-import "./simplify-random-button.js";
 
 createRoot(document.getElementById("root")).render(<App />);
