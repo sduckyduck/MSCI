25f2456c959128715dbca92ca7fd3e5763de9355
sduckyduck <hqywcyhqf@gmail.com>
2026-05-02 11:45:13 -0400

    Load MSCI class visual fix script

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./compact-text.css";
 import "./character-builder.css";
 import "./premium-light.css";
 import "./msio-safe-action-fix.js";
+import "./msci-class-visual-fix.js";
 import "./safe-result-export.js";
 
 createRoot(document.getElementById("root")).render(
