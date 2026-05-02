1710752a951cfc30ed9454328c6089345302832e
sduckyduck <hqywcyhqf@gmail.com>
2026-05-02 14:18:54 -0400

    Reduce result page render loops

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,8 @@ import "./compact-text.css";
 import "./character-builder.css";
 import "./premium-light.css";
 import "./export-stable-preview.css";
-import "./stable-character-preview-manager.js";
 import "./gender-only-character-controls.js";
 import "./intro-layout-cleanup.js";
 import "./simplify-random-button.js";
-import "./safe-result-export.js";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
