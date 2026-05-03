ec783947b967efbe004e64b9b418872628492512
sduckyduck <hqywcyhqf@gmail.com>
2026-05-03 11:11:25 -0400

    Load author widget CSS

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import "./premium-light.css";
 import "./export-stable-preview.css";
 import "./intro-layout.css";
 import "./export-share-card.css";
+import "./author-widget.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
