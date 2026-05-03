a7ac2f55289b7e4936771ea6926ab1c58509340d
sduckyduck <hqywcyhqf@gmail.com>
2026-05-03 19:51:46 -0400

    Add balanced MSCI question-bank proxy

---
+++ src/model/msciV2QuestionBankBalanced.js
@@ -0,0 +1,8 @@
+export {
+  jobDisplayNames,
+  msciV2Questions,
+  traitDisplayNames,
+  secondJobGroups,
+} from "./msciV2QuestionBank.js";
+
+export { scoreMsciV2 } from "./scoreMsciV2Balanced.js";
