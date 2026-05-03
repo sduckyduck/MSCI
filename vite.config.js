7ae50c00b95a2eb4b0ecd0d25584bd1b5288a139
sduckyduck <hqywcyhqf@gmail.com>
2026-05-03 19:55:37 -0400

    Use balanced MSCI model in Vite app

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/MSCI/",
+  resolve: {
+    alias: [
+      {
+        find: "./model/msciV2QuestionBank",
+        replacement: "/src/model/msciV2QuestionBankBalanced.js",
+      },
+    ],
+  },
 });
