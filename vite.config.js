c5d1a7c96f7e67a8916e6f72ceea7c635ebf33dd
sduckyduck <hqywcyhqf@gmail.com>
2026-05-04 09:27:00 -0400

    Update vite config base path

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MSCI/",
+  base: "/",
   resolve: {
     alias: [
       {
         find: "./model/msciV2QuestionBank",
-        replacement: "/src/model/msciV2QuestionBankBalanced.js",
-      },
-    ],
-  },
+        replacement: "/src/model/msciV2QuestionBankBalanced.js"
+      }
+    ]
+  }
 });
